51aea3d174c80d880e73cb7fd84d87a12e1042ca
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-31 10:24:01 -0300

    refactor: adjust imports appointment

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
+import { HttpPostClient } from "../../protocols/http/http-post-client";
 import { RemoteAuthentication } from "./remote-authentication";
 
 class HttpPostClientStub implements HttpPostClient {
