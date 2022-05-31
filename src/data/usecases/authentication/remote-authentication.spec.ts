ea0822c4238f38924639e5da4d2ad483ab18b3b5
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-31 10:18:25 -0300

    test: ensure RemoteAuthentication Usecase calls httpClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,20 @@
+import { HttpPostClient } from "data/protocols/http/http-post-client";
+import { RemoteAuthentication } from "./remote-authentication";
+
+class HttpPostClientStub implements HttpPostClient {
+  public url?: string;
+  async post(url: string): Promise<void> {
+    this.url = url;
+    return Promise.resolve();
+  }
+}
+
+describe('RemoteAuthentication Usecase', () => {
+  test('should call httpClient with correct URL', async () => {
+    const httpPostClientStub = new HttpPostClientStub(); 
+    const url = "any_url";
+    const sut = new RemoteAuthentication(url, httpPostClientStub);
+    await sut.auth();
+    expect(httpPostClientStub.url).toBe(url)
+  });
+});
