1cb5241a63852e794d24c9363929325ce939ae56
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-01 09:37:24 -0300

    feat: ensure RemoteAuthentication Usecase calls httpPostClient with correct body

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,8 +1,11 @@
 export type httpPostParams = {
   url: string;
+  body?: object;
 };
 
 export interface HttpPostClient {
   url?: string;
+  body?: object;
+
   post (params: httpPostParams): Promise<void>;
 }
