78ee11a3b6f65cc5475c75f3cd52c0b7ac2da17d
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-04 10:44:25 -0300

    test: ensure Login Component present valid state if password is valid

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -58,4 +58,13 @@ describe('Login Component', () => {
     const passwordStatus = sut.getByTestId("password-status");
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
   });
+
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+    const emailStatus = sut.getByTestId("email-status");
+    expect(emailStatus.title).toBe("Tudo certo!");
+  });
 });
