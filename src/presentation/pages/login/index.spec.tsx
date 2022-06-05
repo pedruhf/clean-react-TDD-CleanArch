237ceee4c6725e6d1d733ccd5b4090d64b8356f2
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-05 16:40:33 -0300

    test: ensure Login component enable submit button if form is valid

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -76,4 +76,15 @@ describe('Login Component', () => {
     const passwordStatus = sut.getByTestId("password-status");
     expect(passwordStatus.title).toBe("Tudo certo!");
   });
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
+    const submitButton = sut.getByTestId("submit-button") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(false);
+  });
 });
