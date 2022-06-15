8d9c456d5a911f68eca7f7440437799d4161c372
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-15 13:35:49 -0300

    test: ensure MinLength Validation returns error if value is less than 5

---
+++ src/validation/validators/min-length/min-length-valitadion.spec.ts
@@ -0,0 +1,12 @@
+import { MinLengthValidation } from "./min-length-valitadion";
+import { InvalidFieldError } from "@/validation/errors";
+import { faker } from "@faker-js/faker";
+
+describe('MinLength Validation', () => {
+  test('Should return error if value is invalid', () => {
+    const field = faker.random.word();
+    const sut = new MinLengthValidation(field, 5);
+    const error = sut.validate("123");
+    expect(error).toEqual(new InvalidFieldError(field))
+  });
+});
