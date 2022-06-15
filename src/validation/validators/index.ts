137a0fbedf49e736e1c9c5aafd062f05bbe02667
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-15 15:45:03 -0300

    feat: ensure ValidationBuilder has a RequiredFieldValidation

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,5 @@
+export * from "./builder/validation-builder";
+export * from "./email/email-validation";
+export * from "./min-length/min-length-valitadion";
+export * from "./required-fields/required-field-validation";
+export * from "./validation-composite/validation-composite";
