152c821ad343fb18dbf09b56c6fb89bf89fec50b
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-21 14:06:42 -0300

    test: ensure Login Component presents error if SaveAccessToken fails

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -199,6 +199,15 @@ describe('Login Component', () => {
     expect(history.length).toBe(1);
     expect(history.location.pathname).toBe("/");
   });
+  
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut();
+    const error = new InvalidCredentialsError();
+    jest.spyOn(saveAccessTokenMock, "save").mockReturnValueOnce(Promise.reject(error));
+    await simulateValidSubmit(sut);
+    const errorWrap = sut.getByTestId("error-wrap");
+    expect(errorWrap.childElementCount).toBe(1);
+  });
 
   test('Should go to signup page', async () => {
     const { sut } = makeSut();
