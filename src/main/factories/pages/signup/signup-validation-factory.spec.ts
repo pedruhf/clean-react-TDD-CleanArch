d283aad4c5b3b43093560f1acfcdb7f75698438e
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-06-28 09:00:07 -0300

    test: adds signup validation factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationBuilder, ValidationComposite } from "@/validation/validators";
+import { makeSignUpValidation } from "./signup-validation-factory";
+
+describe('SignUpValidation Factory', () => {
+  test('Should compose ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation();
+    expect(composite).toEqual(new ValidationComposite([
+      ...ValidationBuilder.field("name").required().min(3).build(),
+      ...ValidationBuilder.field("email").required().email().build(),
+      ...ValidationBuilder.field("password").required().min(5).build(),
+      ...ValidationBuilder.field("passwordConfirmation").required().min(5).sameAs("password").build(),
+    ]));
+  });
+});
