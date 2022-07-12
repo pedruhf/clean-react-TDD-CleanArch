38853c60e2b81e00fecc044574d84f33f34955c1
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-11 21:12:12 -0300

    test: ensure SignUp presents error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,4 +1,5 @@
 import * as MockFormHelper from "../support/form-helper"
+import { faker } from "@faker-js/faker";
 
 describe("SignUp", () => {
   beforeEach(() => {
@@ -17,6 +18,23 @@ describe("SignUp", () => {
 
     cy.getByTestId("passwordConfirmation").should("have.attr", "readOnly");
     MockFormHelper.testInputStatus("passwordConfirmation", "Campo obrigatório");
+
+    cy.getByTestId("submit-button").should("have.attr", "disabled");
+    cy.getByTestId("error-wrap").should("not.have.descendants");
+  });
+
+  it("Should present error state if form is invalid", () => {
+    cy.getByTestId("name").focus().type(faker.random.alphaNumeric(2));
+    MockFormHelper.testInputStatus("name", "O campo name está invalido");
+
+    cy.getByTestId("email").focus().type(faker.random.word());
+    MockFormHelper.testInputStatus("email", "O campo email está invalido");
+
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(4));
+    MockFormHelper.testInputStatus("password", "O campo password está invalido");
+
+    cy.getByTestId("passwordConfirmation").focus().type(faker.random.alphaNumeric(4));
+    MockFormHelper.testInputStatus("passwordConfirmation", "O campo passwordConfirmation está invalido");
 
     cy.getByTestId("submit-button").should("have.attr", "disabled");
     cy.getByTestId("error-wrap").should("not.have.descendants");
