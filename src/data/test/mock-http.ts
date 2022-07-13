22762cdd6d23cc3109b4b48dcd777a9c87c0b67f
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-13 11:41:13 -0300

    feat: ensure RemoteLoadSurveyList throws UnexpectedError if HttpGetClient returns 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,9 +1,13 @@
-import { HttpGetClient, HttpGetParams } from "@/data/protocols/http";
+import { HttpGetClient, HttpGetParams, HttpResponse, HttpStatusCode } from "@/data/protocols/http";
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<R> implements HttpGetClient<R> {
   url: string;
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok,
+  };
 
-  async get (params: HttpGetParams): Promise<void> {
+  async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url;
+    return this.response;
   }
 }
--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,6 +1,7 @@
 import { SurveyModel } from "@/domain/models";
 import { LoadSurveyList } from "@/domain/usecases";
-import { HttpGetClient } from "@/data/protocols/http";
+import { UnexpectedError } from "@/domain/errors";
+import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor (
@@ -8,8 +9,12 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
     private readonly httpGetClient: HttpGetClient,
   ) {}
 
-  async loadAll (): Promise<SurveyModel[]> {
-    await this.httpGetClient.get({ url: this.url });
-    return;
+  async loadAll (): Promise<void> {
+    const httpResponse = await this.httpGetClient.get({ url: this.url });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break;
+      default: throw new UnexpectedError();
+    }
   }
 }
