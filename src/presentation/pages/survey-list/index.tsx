31b4d6a73698ac2b05e4f3e681c3b1217c6b99f2
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-19 13:16:47 -0300

    feat: adds survey icon to show status

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -20,28 +20,9 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={styles.surveyContent}>
-              <time>
-                <span className={styles.day}>19</span>
-                <span className={styles.month}>07</span>
-                <span className={styles.year}>2022</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={styles.surveyContent}>
-              <time>
-                <span className={styles.day}>19</span>
-                <span className={styles.month}>07</span>
-                <span className={styles.year}>2022</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={styles.surveyContent}>
+              <div className={[styles.iconWrap, styles.green].join(" ")}>
+              <img className={styles.icon} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" alt="thumb up" />
+              </div>
               <time>
                 <span className={styles.day}>19</span>
                 <span className={styles.month}>07</span>
