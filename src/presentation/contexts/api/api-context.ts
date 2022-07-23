3778886bec498b240a226f4f6acb0d5c666b0284
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-22 21:02:02 -0300

    feat: adds getCurrentAccount to api context

--- src/presentation/contexts/api/api-context.ts
+++ src/presentation/contexts/api/api-context.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 
 type ApiContextProps = {
   setCurrentAccount?: (account: AccountModel) => void;
+  getCurrentAccount?: () => AccountModel;
 };
 
 export default createContext<ApiContextProps>(null);
