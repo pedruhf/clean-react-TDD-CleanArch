8bbe0ab79bc54ced1cf4601209044183cc5e5604
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-24 11:54:01 -0300

    test: ensure SurveyList Component calls LoadSurveyList on start

--- src/presentation/pages/survey-list/index.spec.tsx
+++ src/presentation/pages/survey-list/index.spec.tsx
@@ -2,9 +2,31 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import { SurveyList } from "@/presentation/pages";
+import { LoadSurveyList } from "@/domain/usecases";
+import { SurveyModel } from "@/domain/models";
 
-const makeSut = () => {
-  render(<SurveyList />);
+
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+  
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++;
+    return Promise.resolve([]);
+  }
+}
+
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />);
+
+  return {
+    loadSurveyListSpy,
+  };
 };
 
 describe('SurveyList Component', () => {
@@ -12,5 +34,10 @@ describe('SurveyList Component', () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+  });
+
+  test('Should call LoadSurveyList on start', () => {
+    const { loadSurveyListSpy } = makeSut();
+    expect(loadSurveyListSpy.callsCount).toBe(1);
   });
 });
