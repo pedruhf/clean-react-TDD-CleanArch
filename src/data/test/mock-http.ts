919f306d017caf839316cdca83a7bb06583d9eb1
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-25 11:13:09 -0300

    feat: ensure AuthorizeHttpGetClient Decorator not adds headers if GetStorage is invalid

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -2,14 +2,16 @@ import { faker } from "@faker-js/faker";
 
 import { HttpGetClient, HttpGetParams, httpPostParams, HttpResponse, HttpStatusCode } from "@/data/protocols/http";
 
-export class HttpGetClientSpy<R> implements HttpGetClient<R> {
+export class HttpGetClientSpy<R = any> implements HttpGetClient<R> {
   url: string;
+  headers?: any;
   response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok,
   };
 
   async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url;
+    this.headers = params.headers;
     return this.response;
   }
 }
