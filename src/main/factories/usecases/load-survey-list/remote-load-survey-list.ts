edb77e97a0e0255c7f9525f38cd08b3e2fe64b0c
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-07-27 11:35:15 -0300

    feat: adds AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,7 +1,8 @@
-import { RemoteLoadSurveyList } from "@/data/usecases/load-survey-list/remote-load-survey-list";
+import { RemoteLoadSurveyList } from "@/data/usecases";
 import { LoadSurveyList } from "@/domain/usecases";
-import { makeApiUrl, makeAxiosHttpClient } from "@/main/factories/http";
+import { makeApiUrl } from "@/main/factories/http";
+import { makeAuthorizeHttpGetClientDecorator } from "@/main/factories/decorators";
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl("/surveys"), makeAxiosHttpClient());
+  return new RemoteLoadSurveyList(makeApiUrl("/surveys"), makeAuthorizeHttpGetClientDecorator());
 };
