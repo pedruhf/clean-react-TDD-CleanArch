7a2a99768339adac4a4a090af40ff77dd6568659
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-01 21:33:46 -0300

    test: ensure logout calls setCurrentAccount with undefined

---
+++ src/presentation/components/header/index.spec.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createMemoryHistory} from "history";
+import { Router } from "react-router-dom";
+
+import { Header } from "@/presentation/components";
+import { ApiContext } from "@/presentation/contexts"
+
+describe('Header Component', () => {
+  test('Should call setCurrentAccount with undefined', () => {
+    const history = createMemoryHistory({ initialEntries: ["/"]});
+    const setCurrentAccountMock = jest.fn();
+    render(
+      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </ApiContext.Provider>
+    );
+    fireEvent.click(screen.getByTestId("logout"));
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
+    expect(history.location.pathname).toBe("/login");
+  });
+});
--- src/presentation/pages/survey-list/index.spec.tsx
+++ src/presentation/pages/survey-list/index.spec.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 import { SurveyList } from "@/presentation/pages";
 import { LoadSurveyListSpy } from "@/domain/test";
 import { UnexpectedError } from "@/domain/errors";
+import { ApiContext } from "@/presentation/contexts";
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy;
 };
 
 const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
-  render(<SurveyList loadSurveyList={loadSurveyListSpy} />);
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn() }}>
+        <Router history={createMemoryHistory()}>
+          <SurveyList loadSurveyList={loadSurveyListSpy} />
+        </Router>
+      </ApiContext.Provider>
+  );
 
   return {
     loadSurveyListSpy,
