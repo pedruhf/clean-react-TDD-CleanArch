a22dfc38e8a984f37d71a7ab03d8bc26d6742a9c
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-01 21:14:56 -0300

    feat: ensure LocalStorageAdapter calls removeItem if value is falsy

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -2,6 +2,10 @@ import { GetStorage, SetStorage } from "@/data/protocols/cache";
 
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   set (key: string, value: object): void {
+    if (!value) {
+      localStorage.removeItem(key);
+      return;
+    }
     localStorage.setItem(key, JSON.stringify(value));
   }
 
