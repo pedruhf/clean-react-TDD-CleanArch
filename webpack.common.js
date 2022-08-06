5e63cb23e270f18ee310ad7ab4548c961df6f40b
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-06 17:04:08 -0300

    feat: adds node server to serve static files

--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,11 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: "./src/main/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist",
+    path: path.join(__dirname, "dist"),
     filename: "main-bundle-[hash].js"
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", ".scss"],
     alias: {
       "@": path.join(__dirname, "src")
     }
