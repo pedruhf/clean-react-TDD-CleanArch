656a443bf2ffc3565ae379c8441079bfd55ba664
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-11 21:50:48 -0300

    feat: adds SurveyResult composition

---
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { SurveyResult } from "@/presentation/pages";
+import { makeRemoteLoadSurveyResult } from "@/main/factories/usecases";
+import { useParams } from "react-router-dom";
+
+export const makeSurveyResult: React.FC = () => {
+  const { id } = useParams<any>();
+  return (
+    <SurveyResult loadSurveyResult={makeRemoteLoadSurveyResult(id)} />
+  )
+};
