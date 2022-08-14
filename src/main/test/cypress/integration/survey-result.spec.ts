ace3c5aba6471c1811da04bec8b564482f372224
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-14 19:31:44 -0300

    test: ensure SurveyResult reload on button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -7,6 +7,10 @@ const mockUnexpectedError = (): void => {
   MockHttpHelper.mockServerError(path, "GET");
 };
 
+const mockSuccess = (): void => {
+  MockHttpHelper.mockOk(path, "GET", "fx:survey-result");
+};
+
 describe("SurveyResult", () => {
   beforeEach(() => {
     cy.fixture("account").then(account => {
@@ -14,9 +18,18 @@ describe("SurveyResult", () => {
     });
   });
 
-  it("Should present error on unexpectedError", () => {
+  it("Should present error on UnexpectedError", () => {
     mockUnexpectedError();
     cy.visit("/surveys/any_id");
     cy.getByTestId("error").should("contain.text", "Erro inesperado. Tente novamente em instantes");
+  });
+
+  it("Should reload on button click", () => {
+    mockUnexpectedError();
+    cy.visit("/surveys/any_id");
+    cy.getByTestId("error").should("contain.text", "Erro inesperado. Tente novamente em instantes");
+    mockSuccess();
+    cy.getByTestId("reload-button").click();
+    cy.getByTestId("question").should("exist");
   });
 });
