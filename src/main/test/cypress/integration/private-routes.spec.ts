1fc191a2c373c4bd28354b96f1a2b54cf342d273
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-14 19:24:43 -0300

    test: ensure SurveyResult redirects to login if no accessToken is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,8 +1,13 @@
 import * as MockHelper from "../utils/helpers";
 
 describe("Private Routes", () => {
-  it("Should logout if surveyList has no accessToken", () => {
+  it("Should logout if SurveyList has no accessToken", () => {
     cy.visit("/");
+    MockHelper.testUrl("/login");
+  });
+
+  it("Should logout if SurveyResult has no accessToken", () => {
+    cy.visit("/surveys/any_id");
     MockHelper.testUrl("/login");
   });
 });
